d90ec14d0f4db1d0b562da27211430fa3450381e
no1_tx <no1@no1sg.ru>
2025-07-10 18:46:27 +0300

    fix

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -55,5 +55,5 @@
       RemainAfterExit = true;
     };
   };
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = true;
 }
