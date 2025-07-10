726f775655d0bb492d8dafe5439b2bc37d57a71c
no1_tx <no1@no1sg.ru>
2025-07-10 18:20:43 +0300

    fix d3cold and import quirks from nixos-hardware

--- hosts/macbook.nix
+++ hosts/macbook.nix
@@ -15,4 +15,51 @@
     pulse.enable = true;
     jack.enable = false;
   };
+
+  # Touchpad/keyboard quirks (libinput overrides)
+  services.libinput.enable = true;
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [MacBook(Pro) SPI Touchpads]
+    MatchName=*Apple SPI Touchpad*
+    ModelAppleTouchpad=1
+    AttrTouchSizeRange=200:150
+    AttrPalmSizeThreshold=1100
+
+    [MacBook(Pro) SPI Keyboards]
+    MatchName=*Apple SPI Keyboard*
+    AttrKeyboardIntegration=internal
+
+    [MacBookPro Touchbar]
+    MatchBus=usb
+    MatchVendor=0x05AC
+    MatchProduct=0x8600
+    AttrKeyboardIntegration=internal
+  '';
+
+  # Firmware, microcode, WiFi
+  networking.enableB43Firmware = true;
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.intel.updateMicrocode = true;
+  };
+
+  # Клавиатура (applespi) и iommu
+  boot.initrd.kernelModules = [
+    "applespi"
+    "spi_pxa2xx_platform"
+    "intel_lpss_pci"
+    "applesmc"
+  ];
+  boot.kernelParams = [ "intel_iommu=on" ];
+
+  # --- d3cold workaround ---
+  systemd.services.disable-nvme-d3cold = {
+    description = "Disables d3cold on the NVME controller";
+    before = [ "suspend.target" ];
+    path = [ pkgs.bash pkgs.coreutils ];
+    serviceConfig.Type = "oneshot";
+    serviceConfig.ExecStart = "${./disable-nvme-d3cold.sh}";
+    serviceConfig.TimeoutSec = 0;
+    wantedBy = [ "multi-user.target" "suspend.target" ];
+  };
 }
