205eed32728954a4ddeb645b07e1b7b0427c3450
no1_tx <no1@no1sg.ru>
2025-07-11 17:44:35 +0300

    fix

--- hosts/packages/macbook/snd-hda-cs8409/default.nix
+++ hosts/packages/macbook/snd-hda-cs8409/default.nix
@@ -1,58 +1,63 @@
-{ stdenv, lib, fetchgit, linuxKernel, kernel ? linuxKernel.kernels.linux_6_12
-, version ? "259cc39e243daef170f145ba87ad134239b5967f" }:
+{ stdenv, lib, fetchgit, kernel ? linuxKernel.kernels.linux_6_12
+, version ? "f30643972f47f63f340e49aadddbcb61729dac03" }:
 
-stdenv.mkDerivation {
-
-  inherit version;
-  name = "snd-hda-codec-cs8409-${version}-module-${kernel.modDirVersion}";
-
-  src = fetchgit {
-    url = "https://github.com/davidjo/snd_hda_macbookpro.git";
+let
+  # Загружаем репо с патчами и модулем
+  moduleSrc = fetchgit {
+    url = "https://github.com/davidjo/snd-hda-macbookpro.git";
     rev = version;
-    sha256 = "sha256-M1dE4QC7mYFGFU3n4mrkelqU/ZfCA4ycwIcYVsrA4MY=";
+    sha256 = "sha256-wZbRcen6uLahb/goX4t2ECtzv8XqZ6kRPN8bp1NTCR0=";
   };
 
-  patches = [
-    ./patch_patch_cs8409.c.diff
-    ./patch_patch_cs8409.h.diff
-    ./patch_patch_cirrus_apple.h.diff
-  ];
+  # Путь к исходникам ядра, где будем патчить
+  kernelSrc = kernel.dev;
 
-  hardeningDisable = [ "pic" ];
+in stdenv.mkDerivation {
+  name = "snd-hda-codec-cs8409-module-${version}-${kernel.modDirVersion}";
+  inherit version;
+
+  # В качестве src берем только файлы из модуля (копируем их отдельно)
+  # Патчи будут применяться к исходникам ядра, а не сюда
+  src = moduleSrc;
+
   nativeBuildInputs = kernel.moduleBuildDependencies;
 
-  NIX_CFLAGS_COMPILE = [ "-g" "-Wall" "-Wno-unused-variable" "-Wno-unused-function" ];
-
+  # Параметры сборки
   makeFlags = kernel.makeFlags ++ [
     "INSTALL_MOD_PATH=$(out)"
     "KERNELRELEASE=${kernel.modDirVersion}"
-    "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "KERNEL_DIR=${kernelSrc}/lib/modules/${kernel.modDirVersion}/build"
   ];
 
-  # Подготовка Makefile и исправление include-путей в patch_cs8409.h
-  postPatch = ''
-    # Создаем Makefile вручную, аналогично скрипту
-    cat > Makefile << EOF
-snd-hda-codec-cs8409-objs := patch_cs8409.o patch_cs8409-tables.o
-obj-$(CONFIG_SND_HDA_CODEC_CS8409) += snd-hda-codec-cs8409.o
+  # На этапе patchPhase: применяем патчи к исходникам ядра в build/sound/pci/hda,
+  # а не к модулю из репо
 
-KBUILD_EXTRA_CFLAGS := -DAPPLE_PINSENSE_FIXUP -DAPPLE_CODECS -DCONFIG_SND_HDA_RECONFIG=1
+  buildPhase = ''
+    # Копируем исходники ядра в локальную папку build
+    mkdir -p build
+    cp -r ${kernelSrc}/source/sound/pci/hda build/
 
-default:
-	\$(MAKE) -C \$(KERNEL_DIR) M=\$(PWD) CFLAGS_MODULE="\$(KBUILD_EXTRA_CFLAGS)"
+    # Применяем патчи к исходникам ядра
+    patch -d build/sound/pci/hda -p1 < ${moduleSrc}/patch_patch_cs8409.c.diff
+    patch -d build/sound/pci/hda -p1 < ${moduleSrc}/patch_patch_cs8409.h.diff
+    patch -d build/sound/pci/hda -p1 < ${moduleSrc}/patch_patch_cirrus_apple.h.diff
 
-install:
-	\$(MAKE) -C \$(KERNEL_DIR) M=\$(PWD) modules_install
-EOF
+    # Копируем файлы модуля из репо в каталог с исходниками ядра, чтобы собрать модуль
+    cp ${moduleSrc}/*.c build/sound/pci/hda/
+    cp ${moduleSrc}/*.h build/sound/pci/hda/
+    cp ${moduleSrc}/Makefile build/sound/pci/hda/
 
-    # Исправляем пути include в patch_cs8409.h для корректной сборки с исходниками ядра
-    sed -i "s|<sound/cs42l42.h>|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/include/sound/cs42l42.h|" patch_cs8409.h
-    sed -i "s|hda_local.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_local.h|" patch_cs8409.h
-    sed -i "s|hda_jack.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_jack.h|" patch_cs8409.h
-    sed -i "s|hda_generic.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_generic.h|" patch_cs8409.h
-    sed -i "s|hda_auto_parser.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_auto_parser.h|" patch_cs8409.h
+    # Переходим в директорию с исходниками ядра
+    cd build/sound/pci/hda
+
+    # Запускаем сборку модуля
+    make KERNELRELEASE=${kernel.modDirVersion} KERNEL_DIR=${kernelSrc}/lib/modules/${kernel.modDirVersion}/build
+
+    # Установка в $(out)
+    make INSTALL_MOD_PATH=$(out) modules_install
   '';
 
+  installPhase = "true";  # все сделано в buildPhase
 
   meta = with lib; {
     description = "Patched snd-hda-codec-cs8409 kernel module for MacBookPro audio";
