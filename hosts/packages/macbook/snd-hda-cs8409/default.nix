0f764be1ca65216dd5a9aaba90f9e19b7ee54128
no1_tx <no1@no1sg.ru>
2025-07-11 17:33:11 +0300

    fix

--- hosts/packages/macbook/snd-hda-cs8409/default.nix
+++ hosts/packages/macbook/snd-hda-cs8409/default.nix
@@ -1,18 +1,22 @@
-{ stdenv, lib, fetchgit, linuxKernel, kernel ? linuxKernel.kernels.linux_6_6
-, version ? "f30643972f47f63f340e49aadddbcb61729dac03" }:
+{ stdenv, lib, fetchgit, linuxKernel, kernel ? linuxPackages_xanmod.ke
+, version ? "259cc39e243daef170f145ba87ad134239b5967f" }:
 
 stdenv.mkDerivation {
 
   inherit version;
-  name = "sna-hda-codec-cs8409-${version}-module-${kernel.modDirVersion}";
-
-  # Upstream: https://github.com/davidjo/snd_hda_macbookpro
+  name = "snd-hda-codec-cs8409-${version}-module-${kernel.modDirVersion}";
 
   src = fetchgit {
-    url = "https://github.com/no1tx/snd-hda-macbookpro.git";
+    url = "https://github.com/davidjo/snd-hda-macbookpro.git";
     rev = version;
-    sha256 = "sha256-wZbRcen6uLahb/goX4t2ECtzv8XqZ6kRPN8bp1NTCR0=";
+    sha256 = "sha256-M1dE4QC7mYFGFU3n4mrkelqU/ZfCA4ycwIcYVsrA4MY=";
   };
+
+  patches = [
+    ./patch_patch_cs8409.c.diff
+    ./patch_patch_cs8409.h.diff
+    ./patch_patch_cirrus_apple.h.diff
+  ];
 
   hardeningDisable = [ "pic" ];
   nativeBuildInputs = kernel.moduleBuildDependencies;
@@ -25,31 +29,35 @@ stdenv.mkDerivation {
     "KERNEL_DIR=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
   ];
 
+  # Подготовка Makefile и исправление include-путей в patch_cs8409.h
   postPatch = ''
-    printf '
-    snd-hda-codec-cs8409-objs := patch_cs8409.o patch_cs8409-tables.o
-    obj-$(CONFIG_SND_HDA_CODEC_CS8409) += snd-hda-codec-cs8409.o
+    # Создаем Makefile вручную, аналогично скрипту
+    cat > Makefile << EOF
+snd-hda-codec-cs8409-objs := patch_cs8409.o patch_cs8409-tables.o
+obj-$(CONFIG_SND_HDA_CODEC_CS8409) += snd-hda-codec-cs8409.o
 
-    KBUILD_EXTRA_CFLAGS = "-DAPPLE_PINSENSE_FIXUP -DAPPLE_CODECS -DCONFIG_SND_HDA_RECONFIG=1"
+KBUILD_EXTRA_CFLAGS := -DAPPLE_PINSENSE_FIXUP -DAPPLE_CODECS -DCONFIG_SND_HDA_RECONFIG=1
 
-    KERNELRELEASE ?= $(shell uname -r)
-    KERNEL_DIR    ?= /lib/modules/$(KERNELRELEASE)/build
-    PWD           := $(shell pwd)
+default:
+	\$(MAKE) -C \$(KERNEL_DIR) M=\$(PWD) CFLAGS_MODULE="\$(KBUILD_EXTRA_CFLAGS)"
 
-    default:
-    	make -C $(KERNEL_DIR) M=$(PWD) CFLAGS_MODULE=$(KBUILD_EXTRA_CFLAGS)
+install:
+	\$(MAKE) -C \$(KERNEL_DIR) M=\$(PWD) modules_install
+EOF
 
-    install:
-    	make -C $(KERNEL_DIR) M=$(PWD) modules_install
-    ' \
-    > Makefile
-
-    sed --in-place 's|<sound/cs42l42.h>|"${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/include/sound/cs42l42.h"|'  patch_cs8409.h
-    sed --in-place 's|hda_local.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_local.h|'                                                      patch_cs8409.h
-    sed --in-place 's|hda_jack.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_jack.h|'                                                        patch_cs8409.h
-    sed --in-place 's|hda_generic.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_generic.h|'                                                  patch_cs8409.h
-    sed --in-place 's|hda_auto_parser.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_auto_parser.h|'                                          patch_cs8409.h
+    # Исправляем пути include в patch_cs8409.h для корректной сборки с исходниками ядра
+    sed -i "s|<sound/cs42l42.h>|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/include/sound/cs42l42.h|" patch_cs8409.h
+    sed -i "s|hda_local.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_local.h|" patch_cs8409.h
+    sed -i "s|hda_jack.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_jack.h|" patch_cs8409.h
+    sed -i "s|hda_generic.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_generic.h|" patch_cs8409.h
+    sed -i "s|hda_auto_parser.h|${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/sound/pci/hda/hda_auto_parser.h|" patch_cs8409.h
   '';
 
-  meta = { platforms = lib.platforms.linux; };
+
+  meta = with lib; {
+    description = "Patched snd-hda-codec-cs8409 kernel module for MacBookPro audio";
+    license = licenses.gpl2Plus;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ no1tx ];
+  };
 }
