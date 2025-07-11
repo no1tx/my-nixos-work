2340de5cb6d94933b795c016a4b66280d2c13d75
no1_tx <no1@no1sg.ru>
2025-07-11 17:34:55 +0300

    fix

--- hosts/packages/macbook/snd-hda-cs8409/default.nix
+++ hosts/packages/macbook/snd-hda-cs8409/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchgit, linuxKernel, kernel ? linuxPackages_xanmod.kernel
+{ stdenv, lib, fetchgit, linuxKernel, kernel ? pkgs.linuxPackages_xanmod.kernel
 , version ? "259cc39e243daef170f145ba87ad134239b5967f" }:
 
 stdenv.mkDerivation {
