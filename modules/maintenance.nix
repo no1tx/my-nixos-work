cc3c65a25e6583efa2ca61f9c6507e6f24ef0a1b
no1_tx <no1@no1sg.ru>
2025-07-14 14:17:29 +0300

    auto update from git

--- modules/maintenance.nix
+++ modules/maintenance.nix
@@ -9,4 +9,26 @@
     dates = "weekly";
     options = "--delete-older-than 30d";
   };
+  systemd.services.git-update-nixos-config = {
+    description = "Update /etc/nixos config from remote git repo";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+      WorkingDirectory = "/etc/nixos";
+      ExecStart = "${pkgs.git}/bin/git pull --rebase && ${pkgs.git}/bin/git fetch --all";
+    };
+  };
+  systemd.timers.git-update-nixos-config = {
+    description = "Timer for updating /etc/nixos config from git";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+    unitConfig = {
+      Requires = [ "systemd-networkd-wait-online.service" ];
+    };
+  };
 }
