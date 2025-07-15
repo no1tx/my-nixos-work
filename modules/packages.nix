f01c72e153a74b85c8573d04af19ebb5c1bcab96
no1_tx <no1@no1sg.ru>
2025-07-15 13:32:44 +0300

    add direnv

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,7 +7,7 @@
     helix vim neovim
     terminus_font beep lnav firefox home-manager virt-manager-qt
     virt-manager syncthing jetbrains.pycharm-community vscode
-    arc-theme mc flashrom dmidecode code-cursor
+    arc-theme mc flashrom dmidecode code-cursor direnv
   ];
   nixpkgs.config.allowUnfree = true;
 
