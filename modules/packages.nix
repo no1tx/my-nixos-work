cdfda201afd7c45e27c0e02db01aa2b12acddb65
no1_tx <no1@no1sg.ru>
2025-07-17 23:18:55 +0300

    add go-tools

--- modules/packages.nix
+++ modules/packages.nix
@@ -4,7 +4,7 @@
     zsh tmux neofetch htop btop tree ripgrep fd bat
     starship curl wget unzip git podman-compose
     python3 poetry gcc gnumake openssh
-    helix vim neovim go
+    helix vim neovim go gopls go-tools
     terminus_font beep lnav firefox home-manager virt-manager-qt
     virt-manager syncthing jetbrains.pycharm-community vscode
     arc-theme mc flashrom dmidecode code-cursor direnv vault
