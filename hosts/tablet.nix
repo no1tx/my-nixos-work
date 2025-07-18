c8a9d80015c08af349056f70eb6f658eb8b33a95
no1_tx <no1@no1sg.ru>
2025-07-18 16:18:03 +0300

    add new toy

---
+++ hosts/tablet.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+{
+  imports = [ 
+    ../hardware-configuration.nix
+  ];
+
+  # --- Сетевые параметры ---
+  networking.hostName = "hp-tablet";
+
+  # --- Bluetooth и прошивки ---
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  hardware.firmware = with pkgs; [
+    linux-firmware
+  ];
+
+  # --- Загрузчик и ядро ---
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+
+  # --- Системные сервисы и обновления ---
+  services.thermald.enable = true;      # Контроль температуры CPU
+  services.fwupd.enable = true;         # Обновление прошивок
+
+  # --- Графическая оболочка и дисплей ---
+  services.displayManager.sddm.enable = true;         # Графический дисплей-менеджер SDDM
+  services.displayManager.sddm.wayland.enable = true; # Поддержка Wayland
+
+  # --- Поведение при закрытии крышки и кнопках питания ---
+  services.logind = {
+    lidSwitch = "suspend";         # При закрытии крышки — suspend
+    lidSwitchDocked = "ignore";   # В доке — игнорировать
+  };
+}
