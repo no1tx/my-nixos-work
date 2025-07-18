95fc23501958f5f347fae49880106d5acdf1ab50
no1_tx <no1@no1sg.ru>
2025-07-18 20:00:26 +0300

    fixup

--- hosts/tablet.nix
+++ hosts/tablet.nix
@@ -60,4 +60,13 @@
       };
     }
   ];
+  
+  systemd.services.fprintd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "simple";
+  };
+
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
 }
